4987117ae424126f8a4a43336f9c553f60bfb02f
mocha <mocha2007andcoco@gmail.com>
2017-06-09 14:26:24 -0400

    M O R E

--- randomsvg.py
+++ randomsvg.py
@@ -3,19 +3,26 @@ sown=random.randint(0,999999)
 random.seed(sown)
 side=800
 #opening
-open("random.svg","w").write('<svg width="'+str(side)+'px" height="'+str(side)+'px" version="1.1" xmlns="http://www.w3.org/2000/svg">\n<desc>')
-open("random.svg","a").write(str(sown)+"</desc>")
+open("/art/"+str(sown)+".svg","w").write('<svg width="'+str(side)+'px" height="'+str(side)+'px" version="1.1" xmlns="http://www.w3.org/2000/svg">\n<desc>')
+open("/art/"+str(sown)+".svg","a").write(str(sown)+"</desc>")
 #how many items do i draw?
 max=random.randint(1,99)
 #stuff
-options=["rect"]
+def rndpos():
+	return str(random.randint(0,side))
+def rndcol():
+	colors=["black","blue","brown","green","grey","navy","none","orange","pink","purple","red","white","yellow"]
+	return random.choice(colors)
+options=["rect","circle","ellipse","line"]
 for i in range(max):
 	c=random.choice(options)
-	if c=="rect":
-		x=random.randint(0,side)
-		y=random.randint(0,side)
-		w=random.randint(0,side)
-		h=random.randint(0,side)
-		open("random.svg","a").write('\n<rect x="'+str(x)+'px" y="'+str(y)+'px" width="'+str(y)+'px" height="'+str(y)+'px"/>')
+	if c=="circle":
+		open("/art/"+str(sown)+".svg","a").write('\n<circle cx="'+rndpos()+'px" cy="'+rndpos()+'px" r="'+rndpos()+'px" fill="'+rndcol()+'"/>')
+	elif c=="ellipse":
+		open("/art/"+str(sown)+".svg","a").write('\n<rect cx="'+rndpos()+'px" cy="'+rndpos()+'px" rx="'+rndpos()+'px" ry="'+rndpos()+'px" fill="'+rndcol()+'"/>')
+	elif c=="rect":
+		open("/art/"+str(sown)+".svg","a").write('\n<rect x="'+rndpos()+'px" y="'+rndpos()+'px" width="'+rndpos()+'px" height="'+rndpos()+'px" fill="'+rndcol()+'"/>')
+	elif c=="line":
+		open("/art/"+str(sown)+".svg","a").write('\n<rect x1="'+rndpos()+'px" y1="'+rndpos()+'px" x2="'+rndpos()+'px" y2="'+rndpos()+'px" stroke="'+rndcol()+'"/>')
 #end
-open("random.svg","a").write('</svg>')
+open("/art/"+str(sown)+".svg","a").write('</svg>')
