813ac3afbb9afcf834b89cd3152edf2a29e90a1f
mocha <mocha2007andcoco@gmail.com>
2017-06-09 19:09:03 -0400

    enhanced

--- newtonsvg.py
+++ newtonsvg.py
@@ -1,31 +1,25 @@
-from cmath import phase
+from cmath import phase,tan,cos
 from math import floor
-name="a=1+i, f(x)=x^2-1"
+name="a=1, f(x)=x^3-1"
 side=800
 coords=2
-step=2
+step=1
 maxcount=20
 pi=3.141592653589793
 #end main definitions
-def root(c):
-	old=c
-	diff=1
-	new=c
+def root(z):
+	oldz=z+1
+	a=1
 	count=0
-	while abs(diff)>.01 and count<maxcount:
-		#THIS IS WHERE TO PUT THE CODE FOR NEWTON
-		a=1+1j
+	while abs(z-oldz)>.01 and count<maxcount:
+		oldz=z
 		try:
-			#new=old-(old**3-1)/(3*old**2)
-			#new=old-(old**3-2*old+2)/(3*old**2-2)
-			new=old-a*(old**2-1)/(2*old)
-		except ZeroDivisionError:
-			new=c
+			z=z-a*(z**3-1)/(3*z**2)#THIS IS WHERE TO PUT THE CODE FOR NEWTON
+		except:
+			print("Warn",z)
 			break
-		diff=new-old
-		old=new
 		count+=1
-	return new
+	return z
 def color(c):
 	theta=phase(root(c))
 	if 0<=theta<=pi/3:#r->y
@@ -56,20 +50,14 @@ def color(c):
 def xy2c(x,y):
 	return x/200-2+(2-y/200)*1j
 def paint():
-	#opening
-	open("art/newton.svg","w+").write('<?xml version="1.0" standalone="no"?>')
-	open("art/newton.svg","a").write('<svg width="'+str(side)+'" height="'+str(side)+'" version="1.1" xmlns="http://www.w3.org/2000/svg">\n<desc>Newton Fractal</desc>')
+	please='<?xml version="1.0" standalone="no"?>\n<svg width="'+str(side)+'" height="'+str(side)+'" version="1.1" xmlns="http://www.w3.org/2000/svg">\n<desc>Newton Fractal</desc>'
 	#how many items do i draw?
-	please=""
 	for column in range(floor(side/step)):
 		print(100*column/floor(side/step),"%")
 		for row in range(floor(side/step)):
-			rgb=color(xy2c(column*step,row*step))
-			please+='\n<rect x="'+str(column*step)+'" y="'+str(row*step)+'" width="'+str(step)+'" height="'+str(step)+'" fill="'+rgb+'"/>'
-			#print(column,row,xy2c(column*step,row*step),root(xy2c(column*step,row*step)))
-	open("art/newton.svg","a").write(please)
+			please+='\n<rect x="'+str(column*step)+'" y="'+str(row*step)+'" width="'+str(step)+'" height="'+str(step)+'" fill="'+color(xy2c(column*step,row*step))+'"/>'
 	#end
-	open("art/newton.svg","a").write('\n<text x="48" y="'+str(side-48)+'" font-family="Verdana" font-size="48">\n\t'+name+'\n</text>')
-	open("art/newton.svg","a").write('\n</svg>')
+	please+='\n<text x="48" y="'+str(side-48)+'" font-family="Verdana" font-size="48">\n\t'+name+'\n</text>\n</svg>'
+	open("art/newton-"+name+".svg","w+").write(please)
 print("Picasso is painting image 0")
 paint()
